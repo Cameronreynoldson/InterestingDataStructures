a6cf0bb8cdb294ef3dfaadbddd6a4b1e734370bb
Cameron Reynoldson <Cameronreynoldson@utexas.edu>
2015-08-18 21:25:32 -0500

    Update LRUcache.java
    https://leetcode.com/problems/lru-cache/

--- LRUcache.java
+++ LRUcache.java
@@ -1,18 +1,23 @@
 import java.util.*;
-
-
-public class LRUcache<K,V> extends LinkedHashMap<K,V>
-{
-	private int limit; 
-
-	public LRUcache(int s)]
-	{
-		super(16,0.75f,true);
-		limit = s; 
-	}
-
-	protected boolean removeEldestEntry(Map.Entry<K,V> eldest)
-	{
-		return size() >= limit;
-	}
+public class LRUCache {
+    Map<Integer,Integer> map; 
+    
+    public LRUCache(int capacity) {
+        map = new LinkedHashMap<Integer,Integer>(16,0.75f,true)
+        {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry eldest)
+                {
+                    return size() > capacity; 
+                }
+        };
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key,-1);
+    }
+    
+    public void set(int key, int value) {
+        map.put(key,value);
+    }
 }
